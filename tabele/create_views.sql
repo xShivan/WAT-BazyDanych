e00c512a3d51c73cc59e3f67bb2941067b58d15c
Michał Cywiński <mcywinski@hotmail.com>
2014-01-22 03:01:47 +0100

    Widok zmaterializowany

--- tabele/create_views.sql
+++ tabele/create_views.sql
@@ -9,3 +9,27 @@ SELECT CONCAT(l.imie, " ", l.nazwisko) "Lekarz", w.data_wizyty "Data wizyty", CO
 FROM lekarz l, wizyta w, pacjent p
 WHERE (w.id_lekarz = l.id_lekarz) AND (w.id_pacjent = p.id_pacjent)
 ORDER BY p.id_pacjent, w.data_wizyty;
+
+delimiter //
+CREATE PROCEDURE informacje_o_recepcie(
+	IN in_id_recepta int
+)
+BEGIN
+	DROP TABLE IF EXISTS recepta_info;
+	CREATE TABLE widok_w_recepcie(
+		wystawiajacy varchar(255),
+		pacjent varchar(255),
+		data_wystawienia datetime,
+		liczba_lekow int
+	);
+	INSERT INTO widok_w_recepcie(wystawiajacy, pacjent, data_wystawienia, liczba_lekow)
+	VALUES (
+		(SELECT CONCAT(l.imie, ' ', l.nazwisko) FROM lekarz l WHERE id_lekarz = (SELECT id_lekarz FROM recepta WHERE id_recepta = in_id_recepta)),
+		(SELECT CONCAT(p.imie, ' ', p.nazwisko) FROM pacjent p WHERE id_pacjent = (SELECT id_pacjent FROM recepta WHERE id_recepta = in_id_recepta)),
+		(SELECT data_wystawienia FROM recepta WHERE id_recepta = in_id_recepta),
+		(SELECT COUNT(*) FROM recepta_lek_relation WHERE id_recepta = in_id_recepta)
+	);
+
+END
+//
+delimiter ;
